40da74aa01331ffe7be93f701bdd258821358141
trun <e.s.trunov@gmail.com>
2026-02-09 17:12:53 +0700

    task_manager

---
+++ task_manager/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "index.html")
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", index),
+]
