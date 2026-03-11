aeec092a6605b151f5f1071f80cec4ae49f16b7b
trun <e.s.trunov@gmail.com>
2026-03-11 14:43:31 +0700

    task_manager

--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
+from django.shortcuts import redirect
 
 from .models import Label
 
