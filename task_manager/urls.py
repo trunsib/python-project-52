359122df33384d2f7eeba7ad6f725e9da135b236
trun <e.s.trunov@gmail.com>
2026-03-12 12:13:51 +0700

    task_manager

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
-
-app_name = "task_manager"
+from task_manager.views import IndexView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    path("", TemplateView.as_view(template_name="index.html"), name="index"),
-
+    path("", IndexView.as_view(), name="index"),
     path("users/", include("task_manager.users.urls")),
     path("statuses/", include("task_manager.statuses.urls")),
     path("tasks/", include("task_manager.tasks.urls")),
