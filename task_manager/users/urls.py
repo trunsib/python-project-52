ebb67c092bb0c0b4d95b8f513e9ba87de39e980c
trun <e.s.trunov@gmail.com>
2026-03-12 19:44:44 +0700

    task_manager

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'users'
+app_name = "users"
 
 urlpatterns = [
-    path('create/', views.UserCreateView.as_view(), name='create'),
-    path('<int:pk>/update/', views.UserUpdateView.as_view(), name='update'),
-    path('<int:pk>/delete/', views.UserDeleteView.as_view(), name='delete'),
+    path("", views.UserListView.as_view(), name="list"),
+    path("create/", views.UserCreateView.as_view(), name="create"),
+    path("<int:pk>/update/", views.UserUpdateView.as_view(), name="update"),
+    path("<int:pk>/delete/", views.UserDeleteView.as_view(), name="delete"),
 ]
