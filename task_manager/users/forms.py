62d1cab3bfde838f6fc22cb365d143492507987d
trun <e.s.trunov@gmail.com>
2026-03-12 17:34:12 +0700

    task_manager

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -2,34 +2,46 @@ from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
-class UserCreateForm(forms.ModelForm):
+class UserRegisterForm(forms.ModelForm):
+    first_name = forms.CharField(
+        label="Имя",
+        max_length=30,
+        widget=forms.TextInput(attrs={'placeholder': 'Введите имя'})
+    )
+    last_name = forms.CharField(
+        label="Фамилия",
+        max_length=30,
+        widget=forms.TextInput(attrs={'placeholder': 'Введите фамилию'})
+    )
+    username = forms.CharField(
+        label="Имя пользователя",
+        max_length=150,
+        widget=forms.TextInput(attrs={'placeholder': 'Введите имя пользователя'})
+    )
     password = forms.CharField(
         label="Пароль",
-        widget=forms.PasswordInput,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Введите пароль'})
     )
     password_confirm = forms.CharField(
         label="Подтверждение пароля",
-        widget=forms.PasswordInput,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Повторите пароль'})
     )
 
     class Meta:
         model = User
-        fields = ["first_name", "last_name", "username", "password"]
+        fields = ['first_name', 'last_name', 'username', 'password']
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("Имя пользователя уже занято")
+        return username
 
     def clean(self):
         cleaned_data = super().clean()
         password = cleaned_data.get("password")
         password_confirm = cleaned_data.get("password_confirm")
-
         if password and password_confirm and password != password_confirm:
             raise ValidationError("Пароли не совпадают")
-
         return cleaned_data
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password"])
-        if commit:
-            user.save()
-        return user
     
