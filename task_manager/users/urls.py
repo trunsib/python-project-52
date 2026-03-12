9fe1b86a2ddbd204ea4d69f787c5e365303720b5
trun <e.s.trunov@gmail.com>
2026-03-12 18:47:08 +0700

    task_manager

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,21 +1,6 @@
 from django.urls import path
-from .views import (
-    UserListView,
-    UserCreateView,
-    UserUpdateView,
-    UserDeleteView,
-    LoginView,
-    LogoutView,
-)
-
-app_name = 'users'
+from .views import register_view
 
 urlpatterns = [
-    path('', UserListView.as_view(), name='list'),
-    path('create/', UserCreateView.as_view(), name='create'),
-    path('<int:pk>/update/', UserUpdateView.as_view(), name='update'),
-    path('<int:pk>/delete/', UserDeleteView.as_view(), name='delete'),
-
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path("register/", register_view, name="register"),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,59 +1,22 @@
-from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth.views import LoginView as DjangoLoginView, LogoutView as DjangoLogoutView
-from django import forms
+from django.contrib import messages
 
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+        password_confirm = request.POST.get("passwordConfirm", "")
 
-class UserCreateForm(forms.ModelForm):
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+        if password != password_confirm:
+            messages.error(request, "Passwords do not match")
+            return render(request, "users/register.html")
 
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'username', 'password']
-        labels = {
-            'first_name': 'Имя',
-            'last_name': 'Фамилия',
-            'username': 'Логин',
-        }
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists")
+            return render(request, "users/register.html")
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data['password'])
-        if commit:
-            user.save()
-        return user
+        User.objects.create_user(username=username, password=password)
+        return redirect("login")  # предполагается, что есть login view
 
-
-class UserListView(ListView):
-    model = User
-    template_name = 'users/list.html'
-
-
-class UserCreateView(CreateView):
-    model = User
-    form_class = UserCreateForm
-    template_name = 'users/form.html'
-    success_url = reverse_lazy('users:login')
-
-
-class UserUpdateView(UpdateView):
-    model = User
-    fields = ['first_name', 'last_name', 'username']
-    template_name = 'users/form.html'
-    success_url = reverse_lazy('users:list')
-
-
-class UserDeleteView(DeleteView):
-    model = User
-    template_name = 'users/confirm_delete.html'
-    success_url = reverse_lazy('users:list')
-
-
-class LoginView(DjangoLoginView):
-    template_name = 'users/login.html'
-
-
-class LogoutView(DjangoLogoutView):
-    next_page = reverse_lazy('users:login')
-    
+    return render(request, "users/register.html")
