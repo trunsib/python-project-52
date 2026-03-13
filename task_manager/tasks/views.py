3a83fc5e7867fa87d62bf745314d0d2c24a582df
trun <e.s.trunov@gmail.com>
2026-03-13 15:25:15 +0700

    task_manager

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -2,32 +2,36 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-# Страница логина
+
+def home(request):
+    return render(request, "base.html")
+
+
 def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect("home")  # перенаправление после успешного входа
+            return redirect("home")
     else:
         form = AuthenticationForm()
+
     return render(request, "tasks/login.html", {"form": form})
 
 
-# Страница регистрации
 def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("login")  # после регистрации на страницу логина
+            return redirect("login")
     else:
         form = UserCreationForm()
+
     return render(request, "tasks/register.html", {"form": form})
 
 
-# Страница выхода
 def logout_view(request):
     logout(request)
     return redirect("login")
