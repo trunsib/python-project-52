327c851e3f80264d3fc83876ec362893e35f58d0
trun <e.s.trunov@gmail.com>
2026-03-13 10:23:12 +0700

    task_manager

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,17 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 
-class IndexView(TemplateView):
-    template_name = "index.html"
-    
+
+def create(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Пользователь успешно зарегистрирован")
+            return redirect("/login/")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "users/form.html", {"form": form})
