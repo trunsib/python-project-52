5a64bcbdea7a8a0a1e634bed3229cd08f0bd48af
trun <e.s.trunov@gmail.com>
2026-03-25 12:19:01 +0700

    task_manager

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 class Status(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100, unique=True, verbose_name='Название')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
     
     def __str__(self):
         return self.name
