72833426c7b8eb53da55772423fc74ac6480cc80
trun <e.s.trunov@gmail.com>
2026-03-25 17:20:33 +0700

    task_manager/urls.pt

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('', include('task_manager.tasks.urls')),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('statuses/', include('task_manager.statuses.urls')),
     path('labels/', include('task_manager.labels.urls')),
     path('admin/', admin.site.urls),
+    path('login/', lambda request: redirect('/users/login/', permanent=False)),
 ]
