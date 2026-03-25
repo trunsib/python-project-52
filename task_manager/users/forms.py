c8f6ba9524200b5918381a563b1e6f9e04009d28
trun <e.s.trunov@gmail.com>
2026-03-25 10:35:06 +0700

    task_manager

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -20,8 +20,9 @@ class UserRegisterForm(UserCreationForm):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Добавляем CSS классы для всех полей
         self.fields['username'].widget.attrs.update({'class': 'form-control'})
         self.fields['password1'].widget.attrs.update({'class': 'form-control'})
         self.fields['password2'].widget.attrs.update({'class': 'form-control'})
         
+        self.fields['password1'].help_text = None
+        self.fields['password2'].help_text = None
