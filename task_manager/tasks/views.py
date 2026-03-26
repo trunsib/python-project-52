7a1ffb2462b4904e7343ebde4e5b48c4b18eb27a
trun <e.s.trunov@gmail.com>
2026-03-26 10:09:19 +0700

    task_manager

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    DetailView
+    )
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
